145544a49e79ebf30ee243bf4a259350afb0efd7
Rach <90192629+Rachiey@users.noreply.github.com>
2023-10-09 13:38:00 +0100

    removed unecessary files

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import './style.css';
-import { Link } from 'react-router-dom';
-import { CharacterCard } from '../../components/';
-import logo from '../../../src/kumaflix.png';
 
 export const Home = () => {
 
